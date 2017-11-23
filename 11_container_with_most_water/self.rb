026d05b5cb4d6684e5510cdd6646de181ef6648e
Hiroyuki Inoue <hiroyuki-inoue@cookpad.com>
2017-11-23 01:37:30 -0800

    Implemented two patterns for prob. 11, but not passed

--- 11_container_with_most_water/self.rb
+++ 11_container_with_most_water/self.rb
@@ -9,8 +9,34 @@
 
 # Note: You may not slant the container and n is at least 2.
 
+require 'pp'
+require 'pry'
+
 # @param {Integer[]} height
 # @return {Integer}
 def max_area(height)
-    
+  hash = {}
+  # ary = []
+  height.each_with_index do |h,i|
+    #hash.store(h, i)
+    hash.store(i,h)
+  end
+  top1, top2 = hash.sort_by {|k,v| v }.reverse[0..1]
+
+  pp hash
+  puts "top1: #{top1}"
+  puts "top2: #{top2}"
+
+  h = top2[1]
+  w = (top1[0] - top2[0]).abs
+  puts "h: #{h}, w: #{w}"
+  h*w
 end
+
+# ary = [1,1] #=> 1
+# ary = [2,2] #=> 2
+# ary = [3,2,5] #=> 6
+# ary = [128, 2437, 32, 33, 321, 2344, 98, 99, 0, 0]
+# ary = [0,0]
+ary = [1,2,1] #=> 2
+puts max_area(ary)
