7a4ee7591b4ab54bb4a837377330c53c0d7b8c50
Hiroyuki Inoue <mammymax@gmail.com>
2017-12-08 22:31:08 -0800

    Add prob. 181 (accepted)

---
+++ 181_employees_earning_more_than_their_managers/self.sql
@@ -0,0 +1,33 @@
+-- 181. Employees earning more than their managers
+-- https://leetcode.com/problems/employees-earning-more-than-their-managers/
+
+/*
+The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
+
++----+-------+--------+-----------+
+| Id | Name  | Salary | ManagerId |
++----+-------+--------+-----------+
+| 1  | Joe   | 70000  | 3         |
+| 2  | Henry | 80000  | 4         |
+| 3  | Sam   | 60000  | NULL      |
+| 4  | Max   | 90000  | NULL      |
++----+-------+--------+-----------+
+
+Given the Employee table, write a SQL query that finds out employees who earn more than their managers. For the above table, Joe is the only employee who earns more than his manager.
+
++----------+
+| Employee |
++----------+
+| Joe      |
++----------+
+*/
+
+select
+    Name as Employee
+from
+    Employee e1
+    left outer join Employee e2 on e1.ManagerId = e2.Id
+where
+    e1.Salary > e2.Salary
+    and e1.ManagerId is not null
+;
