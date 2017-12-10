360998cdf72115ed7564989639b4cdc06ce3a8db
Hiroyuki Inoue <mammymax@gmail.com>
2017-12-09 17:59:23 -0800

    Add prob. 189

---
+++ 189_rotate_array/self.rb
@@ -0,0 +1,18 @@
+# 189. Rotate Array
+# https://leetcode.com/problems/rotate-array/
+
+# Rotate an array of n elements to the right by k steps.
+
+# For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+
+# Note:
+# Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+
+# Related problem: Reverse Words in a String II
+
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Void} Do not return anything, modify nums in-place instead.
+def rotate(nums, k)
+    
+end
